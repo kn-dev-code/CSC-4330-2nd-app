c66cb79896e49cdbed37fe3962e5cd9f208f540b
gkominas <gabekominas@gmail.com>
2026-09-03 13:58:56 -0500

    UI Changes & Gabriel Page
    Added some misc. changes to the UI of the app as well as added my page to the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,91 @@
 import 'package:flutter/material.dart';
-import './pages/kyle/aboutme.dart';
+import './pages/kyle/aboutme.dart' as kyle;
+import './pages/gabriel/aboutme.dart' as gabriel;
 
-void main() { 
-  runApp(const MyApp()); 
-} 
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget { 
-  const MyApp({super.key}); 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override 
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      title: 'Flutter Demo', 
-      theme: ThemeData( 
-        // 1. FIXED: Added 'ColorScheme' before '.fromSeed'
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), 
-      ), 
-      home: const MyHomePage(title: 'Kyle & Gabriel About Me App'), 
-    ); 
-  } 
-} 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Kyle & Gabriel About Me App'),
+    );
+  }
+}
 
-class MyHomePage extends StatefulWidget { 
-  const MyHomePage({super.key, required this.title}); 
-  final String title; 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
-  @override 
-  State<MyHomePage> createState() => _MyHomePageState(); 
-} 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-class _MyHomePageState extends State<MyHomePage> { 
-  int _counter = 0; 
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  void _incrementCounter() { 
-    setState(() { 
-      _counter++; 
-    }); 
-  } 
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
-  @override 
-  Widget build(BuildContext context) { 
-    return Scaffold( 
-      appBar: AppBar( 
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary, 
-        title: Text(widget.title), 
-      ), 
-      // 2. FIXED: Combined the isolated button and Column into a proper Center layout
-      body: Center( 
-        child: Column( 
-          // 3. FIXED: Added 'MainAxisAlignment' before '.center'
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: [ 
-            const Text('You have pushed the button this many times:'), 
-            Text( 
-              '$_counter', 
-              style: Theme.of(context).textTheme.headlineMedium, 
-            ), 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
             const SizedBox(height: 25),
             ElevatedButton(
-              onPressed: () { 
+              onPressed: () {
                 Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: (context) => const AboutMePage()),
-                ); 
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const gabriel.AboutMePage(),
+                  ),
+                );
               },
-              child: const Text('Kyle About Me Page'), 
-            ), 
-          ], 
-        ), 
-      ), 
-      floatingActionButton: FloatingActionButton( 
-        onPressed: _incrementCounter, 
-        tooltip: 'Increment', 
-        child: const Icon(Icons.add), 
-      ), 
-    ); 
-  } 
+              child: const Text('Gabriel About Me Page'),
+            ),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const kyle.AboutMePage(),
+                  ),
+                );
+              },
+              child: const Text('Kyle About Me Page'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
